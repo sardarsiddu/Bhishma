1b349bf80b726bf55c3f2e3535fa6089a3348d79
bhishma <sardarnaidu423@gmail.com>
2021-06-28 13:15:21 +0530

    hello project

--- src/com/cognizant/source/HelloWorld.java
+++ src/com/cognizant/source/HelloWorld.java
@@ -1,11 +1,12 @@
 package com.cognizant.source;
 
-public class HelloWorld {
-
-	public static void main(String[] args) {
-
-           System.out.println("Hello");
-
-	}
-
-}
+public class HelloWorld {  
+public static void main(String[] args) {  
+for(int i=1;i<=5;i++){  
+for(int j=1;j<=i;j++){  
+        System.out.print("* ");  
+}  
+System.out.println();//new line  
+}  
+}  
+}  
